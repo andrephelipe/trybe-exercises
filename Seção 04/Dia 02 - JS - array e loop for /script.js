1a6a285f79a23c6e706705a2bbf7e8f8af733c34
andrephelipe <andre.phelipe.1999@gmail.com>
2022-09-22 15:10:44 -0300

    aula de array / para fixar

--- Seção 04/Dia 02 - JS - array e loop for /script.js
+++ Seção 04/Dia 02 - JS - array e loop for /script.js
@@ -16,7 +16,7 @@ console.log(pizza4);
 console.log(pizza5);
 */
 
-//Array
+//ARRAY
 
 // INDICE         0            1         2          3         4
 let pizzas = ["4 queijos", "Frango", "Caipira", "palmito", "carne"];
@@ -29,23 +29,35 @@ pizzas[5] = "Peito de peru";
 //Colocar um valor no lugar que ja tem. OBS: Ele subescreve o valor em cima do antigo, com const isso nao funciona
 pizzas[0] = "Calabreza";
 
-//Adicionar um valor na ultima posicao --> push
+//Descobrir o tamanho do meu array
+// console.log(pizzas.length);
+
+//Colocar em ordem alfabetica --> numeros, lestras maiusculas contam e caracteres especiais
+// console.log(pizzas.sort());
+
+//Achar o valor pelo indice
+// console.log(pizzas[0]); // indice
+
+//Concatenação
+// console.log("O cardapio é de : " + pizzas);
+
+//Adicionar um valor na ultima posicao --> push()
 pizzas.push("chocolate com morango");
 pizzas.push("4 queijos");
 
-//Descobrir o tamanho do meu array
-console.log(pizzas.length);
+//adicionar um elemento na primeira posicao --> unshift()
+pizzas.unshift("marguerita");
 
-//Colocar em ordem alfabetica --> numeros, lestras maiusculas contam e caracteres especiais
-console.log(pizzas.sort());
+//remover o ultimo elemnto do array --> pop()
+pizzas.pop();
 
-//Achar o valor pelo indice
-console.log(pizzas[0]); // indice
+//remover o primeiro elemento do array --> shif()
+pizzas.shift();
 
-//Concatenação
-console.log("O cardapio é de : " + pizzas);
+//procurar um elemento no aray --> indexOf()
+pizzas.indexOf("palmito") //3
 
-//mostrar pizza por pizza em vez de dentro do array
+//mostrar pizza por pizza em vez de dentro do array manual
 /*
 console.log(pizzas[0]);
 console.log(pizzas[1]);
@@ -56,6 +68,19 @@ console.log(pizzas[5]);
 console.log(pizzas[6]);
 */
 
+//mostrar pizza por pizza em vez de dentro do array automatica com for
+/*
 for (let index = 0; index < pizzas.length; index++) {
   console.log(pizzas[index]);
 }
+*/
+
+//Para fixar: array(listas)
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf("Serviços");
+
+menu.push("Contato");
+
+console.log(menuServices);
+console.log(menu);
