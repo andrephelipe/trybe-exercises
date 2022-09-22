3e33bb91e4cc420e8ad8eb796ac26c32ced12cd5
andrephelipe <andre.phelipe.1999@gmail.com>
2022-09-22 16:03:43 -0300

    aula do for

---
+++ Seção 04/Dia 02 - JS - array e loop for /for.js
@@ -0,0 +1,22 @@
+let tabuada = 8;
+
+for (let i = 1; i <= 10; i+=1) {
+//    console.log(`${i} x ${tabuada} = ` + i * tabuada);
+}
+
+//Parabens para os aprovados 0          1        2         3        4        5
+let listaNomeAprovados = ["Andre", "Phelipe", "Souza", "Santana", "Ana", "Mathias"] // tamanho = 6
+                                        //busca o indice  
+for (let index = 0; index <= listaNomeAprovados.length -4; index+=1) {
+    
+    // console.log(`Parabens ${listaNomeAprovados[index]} pela sua aprovação`);
+    
+} 
+
+// Para fixar --> Imprimir os elementos da lista
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index+=1) {
+    console.log(groceryList[index];)    
+}
