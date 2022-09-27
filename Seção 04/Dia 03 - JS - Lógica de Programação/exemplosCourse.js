9c035cef784f4872ec0c21a6e8937f3b956ee458
andrephelipe <andre.phelipe.1999@gmail.com>
2022-09-27 10:07:20 -0300

    exercicios de logica de programação

---
+++ Seção 04/Dia 03 - JS - Lógica de Programação/exemplosCourse.js
@@ -0,0 +1,37 @@
+// const fruits = [0, 01, 13, 0, 0];
+
+// let soma = 0;
+// for(i=0; i<fruits.length; i++) {
+//     soma += fruits[i];
+//     // soma = soma + fruits[i];
+// }
+
+// if(soma >= 15) {
+//     console.log(soma);
+// } else {
+//     console.log("Valor menor que 16");
+// }
+
+
+
+// function somadorArray(num) {
+//     let soma = 0;
+
+//     for(i=0; i<num; i++) {
+//         soma += num[i];
+//     }
+
+//     return soma;
+
+// }
+// console.log(somadorArray(fruits));
+
+
+//Quantas vezes o numero pode ser subtraido por 5
+// let resultado = 0;
+// for(i=100; i > 0; i-=5) {
+//    if(i >= 0){
+//         resultado++;
+//    }
+// }
+// console.log(resultado);
