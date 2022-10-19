85dfdff97bc09985fde09a6e850acf250985ee46
andrephelipe <andre.phelipe.1999@gmail.com>
2022-10-19 12:56:56 -0300

    object values

--- dia02-fluxo-de-excecao-objetos/5.Object.values/script.js
+++ dia02-fluxo-de-excecao-objetos/5.Object.values/script.js
@@ -1,11 +1,31 @@
 const myDados = {
     name: 'andre',
     age: 23,
-    roby: 'rambo six',
+    roby: 'rombo six',
     date: 'jhenny'
 }
 
+const student = {
+    Html: 'Muito Bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SoftSkill: 'Ótimo',
+  };
+
 // mostra os valores 
 for (const value in myDados) {
-    console.log(myDados[value]);
+    // console.log(myDados[value]);
 }
+
+//Object.values
+console.log(Object.values(myDados));
+
+//object.values => funcao anônima
+const valores = (i) => Object.values(i);
+console.log(valores(myDados));
+console.log(valores(student));
+
+const chaves = (j) => Object.keys(j);
+console.log(chaves(student));
+
+console.log(`${Object.values(myDados)[0]} é ${Object.values(student)[0]}`);
