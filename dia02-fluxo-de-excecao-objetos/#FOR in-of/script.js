c9bed1127f49dff9044640b853b7466ff075c4d8
andrephelipe <andre.phelipe.1999@gmail.com>
2022-10-19 13:06:24 -0300

    for in e for of

--- dia02-fluxo-de-excecao-objetos/#FOR in-of/script.js
+++ dia02-fluxo-de-excecao-objetos/#FOR in-of/script.js
@@ -30,8 +30,14 @@ for (const index in names) {
 // FOR OF
 
 const nam = ['ana', 'beatriz', 'karol', 'gabriel jesus'];
+const nota = [10, 5, 2, 6]
 
 for (const index of nam) {
-    // console.log(index); // mostra os nomes    
+    console.log(`${index} tirou 10 na prova`); // mostra os nomes    
+}
+
+// com for normal
+for(let i = 0; i < nam.length; i++) {
+//    console.log(`${nam[i]} tirou ${nota[i]} na prova`);
 }
 
